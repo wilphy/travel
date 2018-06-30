a5918f03dae33c09d08c5f91adfeca4821393197
wey <149201216@qq.com>
2018-06-30 19:47:04 +0800

    finished city list layout

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="list">
+    <ul class="item">
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    right: 0
+    top: 2.5rem
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <section class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">广州</div>
+          </div>
+        </div>
+      </section>
+      <section class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">广州</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">深圳</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">上海</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">杭州</div>
+          </div>
+        </div>
+      </section>
+      <section class="area">
+        <div class="title border-topbottom">A</div>
+        <ul class="item-list">
+          <li class="item border-bottom">鞍山</li>
+          <li class="item border-bottom">阿拉善盟</li>
+          <li class="item border-bottom">阿坝</li>
+          <li class="item border-bottom">阿克苏</li>
+        </ul>
+      </section>
+      <section class="area">
+        <div class="title border-topbottom">B</div>
+        <ul class="item-list">
+          <li class="item border-bottom">北京</li>
+          <li class="item border-bottom">蚌埠</li>
+          <li class="item border-bottom">博尔塔拉</li>
+          <li class="item border-bottom">保定</li>
+        </ul>
+      </section>
+      <section class="area">
+        <div class="title border-topbottom">C</div>
+        <ul class="item-list">
+          <li class="item border-bottom">北京</li>
+          <li class="item border-bottom">蚌埠</li>
+          <li class="item border-bottom">博尔塔拉</li>
+          <li class="item border-bottom">保定</li>
+        </ul>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+  .border-topbottom
+    &:before
+      border-color: #aaa
+    &:after
+      border-color: #aaa
+  .border-bottom
+  &:before
+    border-color: #bbb
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    .title
+      line-height: .54rem
+      background-color: #eee
+      padding-left: .2rem
+      color: #666
+    .button-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper
+        width:33.33%
+        float: left
+        .button
+          text-align:center
+          margin: .1rem
+          padding: .1rem 0
+          border: .02rem solid #ccc
+          border-radius: .06rem
+    .item-list
+      .item
+        line-height: .76rem
+        padding-left: .2rem
+</style>
